507678df11b0729786112bf96916d2854ba70099
jamclean23 <jamclean23@gmail.com>
2023-10-19 10:56:54 -0400

    Add source maps to webpack config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,6 +7,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,6 +6,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'source-map',
     module: {
         rules: [
             {
