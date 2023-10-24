aa2c1d7ed34526b91e34067f104a901ec40781ac
jamclean23 <jamclean23@gmail.com>
2023-10-24 14:42:13 -0400

    Add layout of homepage

--- server/public/homepage/homepage.js
+++ server/public/homepage/homepage.js
@@ -15,12 +15,31 @@ main();
 // ====== FUNCTIONS ======
 
 function main() {
-  console.log('test10');
+  setCssVars();
   addListeners();
+}
+function setCssVars() {
+  const headerHeight = getHeaderHeight();
+  const navHeight = getNavHeight();
+  const root = document.querySelector(':root');
+  root.style.setProperty('--header-height', `${headerHeight}px`);
+  root.style.setProperty('--nav-height', `${navHeight}px`);
+}
+function getHeaderHeight() {
+  const header = document.querySelector('header');
+  return header.offsetHeight;
+}
+function getNavHeight() {
+  const nav = document.querySelector('.mainContentNav');
+  return nav.offsetHeight;
 }
 function addListeners() {
   const logoutBtn = document.querySelector('#logoutBtn');
   logoutBtn.addEventListener('click', handleLogoutClick);
+  window.addEventListener('resize', handleResize);
+}
+function handleResize() {
+  setCssVars();
 }
 async function handleLogoutClick() {
   window.location.replace(`${window.location.origin}/login/handleLogoutRequest`);
